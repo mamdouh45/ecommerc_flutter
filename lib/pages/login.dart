8c93153ea1549a6ae6d6a2b6adfce6466a2a7a85
mamdouh <3dhousedecor.com@gmail.com>
2020-06-07 19:25:23 +0200

    the test app eccomerce

---
+++ lib/pages/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
