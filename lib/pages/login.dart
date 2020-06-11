02b735bff0a549fc14b6967465dca24a53b0f4aa
mamdouh <3dhousedecor.com@gmail.com>
2020-06-11 19:44:42 +0200

    the test app eccomerce

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -63,53 +63,131 @@ class _LoginState extends State<Login> {
       setState(() {
         loading = true;
       });
-      GoogleSignInAccount googleUser = await googleSignIn.signIn();
-      GoogleSignInAuthentication googleSignInAuthentication =
-          await googleUser.authentication;
-      FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
-          idToken: googleSignInAuthentication.idToken,
-          accessToken: googleSignInAuthentication.accessToken);
-      if (firebaseUser != null) {
-        final QuerySnapshot result = Firestore.instance
-            .collection("users")
-            .where("id", isEqualTo: firebaseUser.uid)
-            .getDocuments() as QuerySnapshot;
-        final List<DocumentSnapshot> documents = result.documents;
-        if (documents.length == 0) {
-          // insert the user to our collection
-          Firestore.instance
-              .collection("users")
-              .document(firebaseUser.uid)
-              .setData({
-            "id":firebaseUser.uid,
-            "username":firebaseUser.displayName,
-            "profilePicture":firebaseUser.photoUrl,
-          });
-          await preferences.setString("id", firebaseUser.uid);
-          await preferences.setString("username", firebaseUser.displayName);
-          await preferences.setString("profilePicture", firebaseUser.photoUrl);
-        }else{
-          await preferences.setString("id", documents[0]['id']);
-          await preferences.setString("username", documents[0]['username']);
-          await preferences.setString("profilePicture", documents[0]['profilePicture']);
-        }
-        Fluttertoast.showToast(msg: "login is successful");
-        setState(() {
-          loading = false;
+      final GoogleSignInAccount googleAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+      await googleAccount.authentication;
 
-        });
-      } else {}
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      try {
+        FirebaseUser user = (await firebaseAuth.signInWithCredential(
+            credential)) as FirebaseUser;
+        return user;
+      } catch (e) {
+        print(e.toString());
+        return null;
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text("login", style: TextStyle(color: Colors.red.shade900),),
+        elevation: 0.1,
+      ),
+      body: Stack(
+        children: [Center(
+          child: FlatButton(
+            color: Colors.red.shade900,
+            onPressed: () {
+              handleSignIn();
+            },
+            child: Text("sign in / Sign up with google ",
+              style: TextStyle(color: Colors.white),),
+          ),
+        ),
+          Visibility(visible: loading ?? true,
+              child: Center(
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.white.withOpacity(0.9),
+                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.red),),
+                ),
+              ))
+        ],
+
+      ),
+    );
+  }
+
+  Future<FirebaseUser>handleSignIn() async {
+     preferences = await SharedPreferences.getInstance();
+
+     setState(() {
+       loading = true;
+     });
+     final GoogleSignInAccount googleAccount = await googleSignIn.signIn();
+     final GoogleSignInAuthentication googleAuth =
+         await googleAccount.authentication;
+
+     final AuthCredential credential = GoogleAuthProvider.getCredential(
+         idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+     try {
+       FirebaseUser user = (await firebaseAuth.signInWithCredential(
+           credential)) as FirebaseUser;
+     return user;
+     } catch (e) {
+     print(e.toString());
+     return null;
+     }
+   }
+   }
+
+/*
+*    loading = true;
+      });
+      GoogleSignInAccount googleAccount = await googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth =
+      await googleAccount.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      try {
+        FirebaseUser user = (await firebaseAuth.signInWithCredential(
+            credential)) as FirebaseUser;
+        return user;
+      } catch (e) {
+        print(e.toString());
+        return null;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text("login", style: TextStyle(color: Colors.red.shade900),),
+        elevation: 0.1,
+      ),
+      body: Stack(
+        children: [Center(
+          child: FlatButton(
+            color: Colors.red.shade900,
+            onPressed: () {
+              handleSignin();
+            },
+            child: Text("sign in / Sign up with google ",
+              style: TextStyle(color: Colors.white),),
+          ),
+        ),
+          Visibility(visible: loading ?? true,
+              child: Center(
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.white.withOpacity(0.9),
+                  child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.red),),
+                ),
+              ))
+        ],
+
+      ),
+    );
   }
 }
-
-@override
-Widget build(BuildContext context) {
-  // TODO: implement build
-  throw UnimplementedError();
-}
+*/
